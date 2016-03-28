bc2a96199bce1a40bb8772398fa8d809b4870950
Vitalii Kulyk <vitcool@ukr.net>
2016-03-28 23:55:05 +0300

    redact some code

--- app/src/main/java/com/example/vitalykulyk/testbinding/MainActivity.java
+++ app/src/main/java/com/example/vitalykulyk/testbinding/MainActivity.java
@@ -4,7 +4,6 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.example.vitalykulyk.testbinding.databinding.MainActivityBinding;
 
 public class MainActivity extends AppCompatActivity {
 
